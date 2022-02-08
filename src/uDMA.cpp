270a5b18e72d1084caea29d16c3c776cab8cdc8b
Trevor Makes <the.trevor.makes@gmail.com>
2022-02-07 20:51:01 -0800

    Remove unused templates merged elsewhere

--- src/uDMA.cpp
+++ src/uDMA.cpp
@@ -12,39 +12,14 @@
 
 namespace uDMA {
 
-// PORTB [D7 D6 D5 D4 D3 D2 D1 D0]
-using Data = uIO::PortB;
-
 // PORTC [* C - - - - - -]
 using Clock = uIO::PinC6;
 
 // PORTD [CS WE - R A3 A2 A1 A0]
-using Address0123 = uIO::PortD::Mask<0b00001111>;
 using Reset = uIO::PinD4;
-using WriteEnable = uIO::PinD6;
-using ChipSelect = uIO::PinD7;
 
 // PORTE [- H - - - * - -]
 using Halt = uIO::PinE6;
-
-// PORTF [A7 A6 A5 A4 - - A9 A8]
-using Address4567 = uIO::PortF::Mask<0b11110000>;
-using Address89 = uIO::PortF::Mask<0b00000011>;
-
-// 2114 SRAM requires 70 ns after chip select before reading data
-template <typename DATA>
-struct DelayRead : DATA {
-  static uint8_t read() {
-    // 2 NOP cycles at 16 MHz delays 125 ns
-    __asm__("nop");
-    __asm__("nop");
-    return DATA::read();
-  }
-};
-
-using TmpControl = Control<ChipSelect, WriteEnable>;
-using Address = uIO::Port16<uIO::PortJoin<Address0123, Address4567>, Address89>;
-using TmpBus = Bus<Address, DelayRead<Data>, TmpControl>;
 
 inline void configure_clock() {
   Clock::config_output(); // DDRC |= bit(6); //< set PC6 (OC3A) as output
